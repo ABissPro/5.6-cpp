1c811e960a33e42b43ade15de7222705422d2d14
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-24 01:28:15 +0300

    Add files via upload

---
+++ Бармен.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+
+int main()
+{
+   int dayOfBirth;
+   int monthOfBirth;
+   int yearOfBirth;
+   std::cout << "Введите дату рождения через пробел: ";
+   std::cin >> dayOfBirth >> monthOfBirth >> yearOfBirth;
+
+   int dayCurrent;
+   int monthCurrent;
+   int yearCurrent;
+   std::cout << "Введите сегодняшнюю дату через пробел: ";
+   std::cin >> dayCurrent >> monthCurrent >> yearCurrent;
+
+   if (yearCurrent - yearOfBirth > 18) {
+	   std::cout << "Можно!";
+   }
+
+   if (yearCurrent - yearOfBirth < 18) {
+	   std::cout << "Нельзя!!";
+   }
+
+   if ((yearCurrent - yearOfBirth) == 18) {
+	   if (monthCurrent == monthOfBirth) {
+		   if (dayCurrent <= dayOfBirth) {
+			   std::cout << "Нельзя!!";
+		   }
+		   else if (dayCurrent > dayOfBirth) {
+			   std::cout << "Можно!";
+		   }
+	   }
+	   else if (monthCurrent <= monthOfBirth) {
+		   std::cout << "Нельзя!!";
+	   }
+	   else if (monthCurrent >= monthOfBirth) {
+		   std::cout << "Можно!";
+	   }
+   }
+}
+
