1027c1204106ba9899c98708e99deba8f3cd85b9
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-24 00:08:59 +0300

    Add files via upload

---
+++ Кирпич.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    std::cout << "Введите параметры первой коробки (a, b, c): ";
+    std::cin >> a >> b >> c;
+
+    int m;
+    int n;
+    int k;
+    std::cout << "Введите параметры второй коробки (m, n, k): ";
+    std::cin >> m >> n >> k;
+
+    if (a <= m && b <= n && c <= k || a <= n && b <= k && c <= m || a <= k && b <= m && c <= n) {
+        std::cout << "Можно!";
+    }
+    
+    else {
+        std::cout << "Нельзя!!";
+    }
+
+}
