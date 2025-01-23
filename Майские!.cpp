3c15a443ea61adede3ed82ae25830168100d4d95
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-23 22:58:42 +0300

    Add files via upload

---
+++ Майские!.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int day;
+    
+
+    std::cout << "Введите день: ";
+    std::cin >> day;
+
+    if ((day % 7 == 0) || ((1 + day) % 7 == 0) || (day >= 1 && day <= 5) || (day >= 8 && day <= 10)) {
+        std::cout << "Этот день - выходной!\n";
+    }
+    else {
+        std::cout << "Этот день рабочий :(";
+    }
+
+}
+
