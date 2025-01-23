3c3c94c23d97347e8274c57e01a18a848ee49db4
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-23 23:44:40 +0300

    Добавлена проверка ввода

--- Майские!.cpp
+++ Майские!.cpp
@@ -9,6 +9,9 @@ int main()
     std::cout << "Введите день: ";
     std::cin >> day;
 
+    if ((day < 1) && (day > 31)) {
+    std::cout << "Ошибка!"
+    }
     if ((day % 7 == 0) || ((1 + day) % 7 == 0) || (day >= 1 && day <= 5) || (day >= 8 && day <= 10)) {
         std::cout << "Этот день - выходной!\n";
     }
