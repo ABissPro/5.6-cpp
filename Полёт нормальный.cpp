d3e5696c0eff65a759c560c3c2fcde538034e368
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-23 16:37:39 +0300

    Add files via upload

---
+++ Полёт нормальный.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int speed;
+    int height;
+
+    std::cout << "Введите скорость самолёта: ";
+    std::cin >> speed;
+
+    std::cout << "Введите высоту самолёта: ";
+    std::cin >> height;
+
+    if ((speed >= 750 && speed <= 850) && (height >= 9000 && height <= 9500)) {
+        std::cout << "Самолёт летит правильным эшелоном!\n";
+    }
+    else {
+        std::cout << "Самолёт летит НЕ правильным эшелоном!!\n";
+    }
+
+}
