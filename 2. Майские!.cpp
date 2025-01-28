fea4f289b9710607b2e56a62b125fa8a34d785db
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-28 21:23:23 +0300

    Update 2. Майские!.cpp

--- 2. Майские!.cpp
+++ 2. Майские!.cpp
@@ -10,7 +10,7 @@ int main()
     std::cin >> day;
 
     if ((day < 1) && (day > 31)) {
-    std::cout << "Ошибка!"
+    std::cout << "Ошибка!";
     }
     if ((day % 7 == 0) || ((1 + day) % 7 == 0) || (day >= 1 && day <= 5) || (day >= 8 && day <= 10)) {
         std::cout << "Этот день - выходной!\n";
